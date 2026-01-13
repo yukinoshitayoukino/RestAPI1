6afecc9325610f8ddb10af6457560d0f0574fd81
kelturax <kelturax@gmail.com>
2026-01-13 08:42:43 -0800

    add class Categories

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
+from email.policy import default
 from symbol import return_stmt
-
 import uvicorn
 from fastapi import FastAPI
 from utils import json_to_dict_list
 import os
 from typing import Optional
+from enum import Enum
+from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError
+import re
 app = FastAPI()
+
+class Categories(str, Enum):
+    trims = "стрижка"
+    coloring = "окрашивание"
+    treatment = "лечение"
+    styling = "укладка"
+    manicure = "маникюр"
+class Services(BaseModel):
+    id: int
+    name: str = Field(default=...,description="название услуги")
+    category: Categories = Field(default=...,description="категория услуг")
+    description: str = Field(default=...,description="описание услуги")
+    price: float = Field(default=...,description="цена услуги")
+    duration_minutes: int = Field(default=...,description="длительность")
+    difficulty_level: int = Field(default=..., ge=0, le=10,description="сложность от нуля до десяти")
+    popularity_score: float = Field(default=..., ge=0, le=10.0,description="популярность услуги, от нуля до десяти")
 
 
 
@@ -40,13 +59,12 @@ def home_page():
 '''
 Основная часть выполнения лабораторной работы
 '''
-@app.get("/services/{service_id}")
-def get_service(service_id: int):
+@app.get("/service/",response_model=Services)
+def get_service_from_param_id(service_id: int):
     services = json_to_dict_list(path_to_json)
-    return_list = []
     for service in services:
         if service["id"] == service_id:
-            return_list.append(service)
-    return return_list
+            return service
+
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
